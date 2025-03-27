14d7cd41fed06341a5de17e00463ea001e782ae7
Eric Banker <EricBanker12@gmail.com>
2025-03-27 08:50:51 -0700

    1.3.0 (#2)
    * ADD: Add count input mode
    * ADJ: minor performance enhancements
    * Update readme and minor changes

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Select all vertices, edges, or faces within an index range.
 ## Usage
 * In Edit Mode, Click on Select -> By Index
 * Modify the selection mode (vertex, edge, face)
-* Modify index selection range
-* Modify selection behavior (replace or add to prior selection)
-* Modify selection range behavior (inclusive vs exclusive stop index)
+* Modify the selection range (start index and stop index or selection count)
+* Modify selection behavior (replace or add to a prior selection)
--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+from itertools import islice
 import bpy
 import bmesh
 
@@ -7,24 +8,8 @@ class SelectByIndex(bpy.types.Operator):
     bl_label = "Select By Index"
     bl_options = {'REGISTER', 'UNDO'}
 
-    # Increment stop when user increments start above stop
-    def update_start(self, context):
-        min_stop = self.start if self.inc_stop_index else self.start + 1
-        if self.stop < min_stop:
-            self.stop = min_stop
-    
-    # Decrement start when user decrements stop below start
-    def update_stop(self, context):
-        max_start = self.stop if self.inc_stop_index else max(self.stop - 1, 0)
-        if self.start > max_start:
-            self.start = max_start
-
-    # Increment or Decrement stop index when changing selection range inclusivity
-    def update_inc_stop_index(self, context):
-        self.stop = max(self.stop - 1, 0) if self.inc_stop_index else self.stop + 1
-
     select_mode: bpy.props.EnumProperty(
-        name="Selection Mode",
+        name="Select Mode",
         description="Choose a selection mode",
         items=[
             ('VERTEX', "Vertex", "Select vertices"),
@@ -33,33 +18,82 @@ class SelectByIndex(bpy.types.Operator):
         ]
     )
 
+    input_mode: bpy.props.EnumProperty(
+        name="Input Mode",
+        description="Choose the selection range inputs",
+        items=[
+            ('INCLUSIVE', "Inclusive", "Input start index and inclusive stop index"),
+            ('EXCLUSIVE', "Exclusive", "Input start index and exclusive stop index"),
+            ('COUNT', "Count", "Input start index and selection count"),
+        ]
+    )
+    
+    count: bpy.props.IntProperty(
+        name="Count",
+        description="The count of items for the selection range",
+        default=1,
+        min=1
+    )
+
+    private_start: bpy.props.IntProperty(default=0, min=0)
+
+    def update_start(self, context):
+        if self.input_mode != 'COUNT':
+            self.count = max(self.count + self.private_start - self.start, 1)
+        self.private_start = self.start
+
     start: bpy.props.IntProperty(
-        name="Selection Start",
+        name="Start",
         description="The starting index for the selection range",
         default=0,
         min=0,
         update=update_start
     )
+
+    def get_inc_stop(self):
+        return self.start + self.count - 1
+
+    def get_exc_stop(self):
+        return self.start + self.count
+
+    def set_inc_stop(self, value):
+        value = max(value, 0)
+        if self.input_mode != 'COUNT' and value < self.start:
+            self.start = value
+            self.count = 1
+        else:
+            self.count = 1 + value - self.start
+
+    def set_exc_stop(self, value):
+        value = max(value, 1)
+        if self.input_mode != 'COUNT' and value <= self.start:
+            self.start = value - 1
+            self.count = 1
+        else:
+            self.count = value - self.start
     
-    stop: bpy.props.IntProperty(
-        name="Selection Stop",
-        description="The ending index for the selection range",
+    inc_stop: bpy.props.IntProperty(
+        name="Stop",
+        description="The inclusive ending index for the selection range",
         default=0,
         min=0,
-        update=update_stop
+        get=get_inc_stop,
+        set=set_inc_stop
+    )
+    
+    exc_stop: bpy.props.IntProperty(
+        name="Stop",
+        description="The exclusive ending index for the selection range",
+        default=1,
+        min=1,
+        get=get_exc_stop,
+        set=set_exc_stop
     )
 
     replace_selection: bpy.props.BoolProperty(
         name="Replace Selection",
         description="Replace instead of adding to the previous selection",
         default=True
-    )
-
-    inc_stop_index: bpy.props.BoolProperty(
-        name="Inclusive Stop Index",
-        description="Make the ending index for the selection range inclusive",
-        default=True,
-        update=update_inc_stop_index
     )
 
     @classmethod
@@ -69,30 +103,44 @@ class SelectByIndex(bpy.types.Operator):
         cls.poll_message_set("The active object must be in Edit mode")
         return False
 
+    def draw(self, context):
+        layout = self.layout
+
+        layout.use_property_split = True
+        layout.use_property_decorate = False
+
+        layout.prop(self, "select_mode")
+        layout.prop(self, "input_mode")
+        layout.prop(self, "start")
+        
+        if self.input_mode == 'INCLUSIVE':
+            layout.prop(self, "inc_stop")
+        elif self.input_mode == 'EXCLUSIVE':
+            layout.prop(self, "exc_stop")
+        elif self.input_mode == 'COUNT':
+            layout.prop(self, "count")
+        
+        layout.prop(self, "replace_selection")
+
     def check(self, context):
         bm = bmesh.from_edit_mesh(context.object.data)
 
         if self.select_mode == 'VERTEX':
-            max_stop = len(bm.verts)
+            max_len = len(bm.verts)
         elif self.select_mode == 'EDGE':
-            max_stop = len(bm.edges)
+            max_len = len(bm.edges)
         elif self.select_mode == 'FACE':
-            max_stop = len(bm.faces)
+            max_len = len(bm.faces)
 
         bm.free()
 
-        max_start = max(max_stop - 1, 0)
-        
-        if self.inc_stop_index:
-            max_stop = max_start
+        max_start = max(max_len - 1, 0)
+        start = self.start
+        stop = self.inc_stop
 
-        if self.start > max_start or self.stop > max_stop:
-            self.start = min(self.start, max_start)
-            self.stop = min(self.stop, max_stop)
-            return True
-        
-        if not self.inc_stop_index and max_stop > 0 and self.stop < 1:
-            self.stop = 1
+        if start > max_start or stop > max_start:
+            self.start = min(start, max_start)
+            self.inc_stop = min(stop, max_start)
             return True
 
         return False
@@ -114,16 +162,13 @@ class SelectByIndex(bpy.types.Operator):
             bpy.ops.mesh.select_mode(type='FACE')
             selectable_items = bm.faces
 
-        start = self.start
-        stop = self.stop + 1 if self.inc_stop_index else self.stop
-        
-        for index, item in enumerate(selectable_items):
-            if index >= start and index < stop:
-                item.select = True
+        for item in islice(selectable_items, self.start, self.exc_stop):
+            item.select = True
         
         bm.select_flush_mode()
         bm.free()
         bmesh.update_edit_mesh(me)
+
         return {'FINISHED'}
         
     def invoke(self, context, event):
